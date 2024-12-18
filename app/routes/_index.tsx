9d1bbbddc34015bc03ebb2e9a12e798dbf325b68
Harshax <basicleo787@gmail.com>
2024-12-18 07:09:57 +0530

    setup a remix-vite-cloudflare and shadcn

---
+++ app/routes/_index.tsx
@@ -0,0 +1,17 @@
+import type { MetaFunction } from "@remix-run/cloudflare";
+import { Button } from "~/components/ui/button";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Spectral-UI" },
+    { name: "description", content: "Welcome to Spectral!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
+}
