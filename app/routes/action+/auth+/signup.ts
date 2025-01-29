38063915e11b6fc507044571484e312b2d8dfb22
leocodeio <basicleo787@gmail.com>
2025-01-29 17:40:54 +0000

    updated code for auth and session

--- app/routes/action+/auth+/signup.ts
+++ app/routes/action+/auth+/signup.ts
@@ -1,28 +1,29 @@
 import { json, type ActionFunctionArgs } from "@remix-run/node";
-import { signin } from "~/services/auth.server";
-import { SigninPayload } from "~/types/user";
+import { signup } from "~/services/auth.server";
+import { SignupPayload } from "~/types/user";
 import { userSession } from "~/services/sessions.server";
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  const signinPayload = {
+  const signupPayload = {
     email: data.email,
     password: data.password,
-  } as SigninPayload;
-  const signinResult = await signin(signinPayload);
-  if (!signinResult) {
+    confirmPassword: data.confirmPassword,
+  } as SignupPayload;
+  const signupResult = await signup(signupPayload);
+  if (signupResult instanceof Error) {
     return json(
-      { success: false, errors: { email: ["Invalid email or password"] } },
+      { success: false, errors: { email: [signupResult.message] } },
       { status: 400 }
     );
   }
-  console.log(signinResult);
+  console.log(signupResult);
   const session = await userSession(request);
-  session.setUserSession(signinResult.user);
+  session.setUserSession(signupResult.user);
   console.log(session.getUserSession());
   return json(
-    { success: true, user: signinResult.user },
+    { success: true, user: signupResult.user },
     {
       headers: {
         "Set-Cookie": await session.commitUserSession(),
--- app/routes/loader+/dashboard+/index.ts
+++ app/routes/loader+/dashboard+/index.ts
@@ -1,16 +1,16 @@
 import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 import { userSession } from "~/services/sessions.server";
 
-export const ROUTE_PATH = "/auth/signin" as const;
+export const ROUTE_PATH = "/dashboard" as const;
 
 export async function loader({
   request,
 }: LoaderFunctionArgs): Promise<Response | null> {
-  // If user is already authenticated, redirect to dashboard
+  // If user is not authenticated, redirect to signin
   const session = await userSession(request);
   const user = session.getUserSession();
-  if (user) {
-    return redirect("/dashboard");
+  if (!user) {
+    return redirect("/auth/signin");
   }
   return null;
 }
