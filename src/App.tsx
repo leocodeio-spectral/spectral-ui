9a03f5be4882d20a0142af6f83894ec2405c2879
leocodeio <basicleo787@gmail.com>
2025-01-31 14:50:42 +0000

    FEAT: added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-blue-500">Hello world!</h1>
-    </>
+    <div>
+      <Button>Click me</Button>
+    </div>
   );
 }
 
