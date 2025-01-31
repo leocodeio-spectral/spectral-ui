b3290af34039de591f9b3573e8de8ff4e3abe0a0
leocodeio <basicleo787@gmail.com>
2025-01-31 17:44:55 +0000

    FEAT: using loader and error boundary, set up not found error

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Landing from "@/routes/Landing";
+import NotFound from "./routes/error/NotFound";
+import AuthLayout from "./routes/auth/AuthLayout";
+import Signup from "./routes/auth/Signup";
+import Signin from "./routes/auth/Signin";
+import AuthIndex from "./routes/auth/AuthIndex";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+    children: [],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      { index: true, element: <AuthIndex /> },
+      { path: "signup", element: <Signup /> },
+      { path: "signin", element: <Signin /> },
+    ],
+  },
+  {
+    path: "*",
+    loader: () => {
+      throw new Response("Not found", { status: 404 });
+    },
+    errorElement: <NotFound />,
+  },
+]);
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
