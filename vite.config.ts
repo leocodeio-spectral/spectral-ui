232ccb169f4c73189274699284dfcac1c74cca6f
leocodeio <basicleo787@gmail.com>
2025-02-07 05:32:05 +0000

    Updated code for remix with working session and all

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { vitePlugin as remix } from "@remix-run/dev";
+import {
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+} from "@remix-run/dev";
 import { flatRoutes } from "remix-flat-routes";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    remixCloudflareDevProxy(),
     remix({
       routes: async (defineRoutes) => {
         return flatRoutes("routes", defineRoutes);
